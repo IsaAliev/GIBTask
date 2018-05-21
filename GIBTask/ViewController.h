09d681b52b9c10ccc9a71bee0f820843bb6a7ee2
IsaAliev <isaaliev12@gmail.com>
2018-05-21 20:16:58 +0300

    Initial Commit

---
+++ GIBTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIBTask
+//
+//  Created by Isa Aliev on 21.05.2018.
+//  Copyright © 2018 IA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
