ee4f1d3c5b51541c892c20e85df5df2dc90f8d17
IsaAliev <isaaliev12@gmail.com>
2018-05-22 16:50:24 +0300

    Clear app delegate

--- GIBTask/AppDelegate.m
+++ GIBTask/AppDelegate.m
@@ -10,8 +10,6 @@
 
 #import <objc/runtime.h>
 
-//[<UITextView 0x7fcd97053a00> keyboardInput:<UITextView 0x7fcd97053a00> shouldInsertText:@"y" isMarkedText:0]
-
 @interface AppDelegate ()
 
 @end
@@ -19,37 +17,8 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//    [UITextView xtrace];
-//    int numClasses;
-//    Class * classes = NULL;
-//
-//    classes = NULL;
-//    numClasses = objc_getClassList(NULL, 0);
-//
-//    if (numClasses > 0 ) {
-//        classes = (__unsafe_unretained Class *)malloc(sizeof(Class) * numClasses);
-//        numClasses = objc_getClassList(classes, numClasses);
-//        for (int i = 0; i < numClasses; i++) {
-//            Class c = classes[i];
-//            if (class_conformsToProtocol(c, @protocol(UIKeyInput))) {
-//                SEL s1 = @selector(insertText:);
-//                SEL s2 = @selector(x_insertText:);
-//
-//                Method originalMethod = class_getInstanceMethod(c, s1);
-//                Method swizzledMethod = class_getInstanceMethod([self class], s2);
-//                
-//                method_exchangeImplementations(swizzledMethod, originalMethod);
-//                NSLog(@"%@ Conforms", NSStringFromClass(c));
-//            }
-//        }
-//        free(classes);
-//    }
 
     return YES;
-}
-
-- (void)x_insertText: (NSString *)text {
-    NSLog(@"INS %@", text);
 }
 
 @end
